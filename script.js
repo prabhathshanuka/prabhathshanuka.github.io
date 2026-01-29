42136877642b70f59a885b7ae6424a1843b016ec
prabhathshanuka <prabhathshanuka@gmail.com>
2026-01-29 22:34:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-// Simple button interaction
-document.querySelectorAll('.btn').forEach(button => {
-  button.addEventListener('click', () => {
-    console.log('Button clicked');
-  });
-});
+const slides = document.querySelectorAll(".slide");
+let index = 0;
+
+setInterval(() => {
+  slides[index].classList.remove("active");
+  index = (index + 1) % slides.length;
+  slides[index].classList.add("active");
+}, 3500);
